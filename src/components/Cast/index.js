ff5122e4c9d341e75c274eb84b792d336da4ca45
Nikhil Mendonca <nikhilmendonca@Nikhils-MacBook-Air.local>
2020-05-19 23:12:15 +0530

    Added redirect to person details page from tv/movie details page

--- src/components/Cast/index.js
+++ src/components/Cast/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { IMAGE_BASE_URL_200, DEFAULT_USER_IMAGE } from "../../constants";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
 	display: inline-block;
@@ -14,19 +15,21 @@ const Character = styled.div`
 	font-weight: 700;
 	white-space: normal;
 	max-width: 100px;
+	color: #000;
 `;
 
 const Name = styled.div`
 	font-size: 10px;
 	font-weight: 500;
 	margin: 4px 0;
+	color: #000;
 `;
 
 const Profile = styled.div`
 	height: 100px;
 	width: 100px;
 	background-image: ${({ url }) =>
-		(url ? `url(${IMAGE_BASE_URL_200}/${url})` : `url(${DEFAULT_USER_IMAGE})`)};
+		url ? `url(${IMAGE_BASE_URL_200}/${url})` : `url(${DEFAULT_USER_IMAGE})`};
 	background-repeat: no-repeat;
 	background-size: contain;
 	background-position: center;
@@ -34,13 +37,19 @@ const Profile = styled.div`
 	border: 1px solid #347768;
 `;
 
+const RouteLink = styled(Link)`
+	text-decoration: none;
+`;
+
 const Cast = ({ cast }) => {
 	return (
-		<Container>
-			<Profile url={cast.profile_path} />
-			<Character>{cast.character}</Character>
-			<Name>{cast.name}</Name>
-		</Container>
+		<RouteLink to={`/person/${cast.id}`}>
+			<Container>
+				<Profile url={cast.profile_path} />
+				<Character>{cast.character}</Character>
+				<Name>{cast.name}</Name>
+			</Container>
+		</RouteLink>
 	);
 };
 
