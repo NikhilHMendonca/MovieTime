9c5857422e38044f73a1da9240f1b76d69486b9e
System Administrator <root@Nikhils-MacBook-Air.local>
2020-05-23 20:20:56 +0530

    Updates

---
+++ precache-manifest.ecedd14e1fe4b997b44d555e9bb061fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "959a286038e501e88930bced5b8c69c5",
+    "url": "/MovieTime/index.html"
+  },
+  {
+    "revision": "f8d16637e4b01999e09a",
+    "url": "/MovieTime/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "2d1c1779289c9f7a9233",
+    "url": "/MovieTime/static/css/main.80f4c2df.chunk.css"
+  },
+  {
+    "revision": "f8d16637e4b01999e09a",
+    "url": "/MovieTime/static/js/2.32e2989d.chunk.js"
+  },
+  {
+    "revision": "2d1c1779289c9f7a9233",
+    "url": "/MovieTime/static/js/main.6a8c0382.chunk.js"
+  },
+  {
+    "revision": "a82b15da297bdfc1de00",
+    "url": "/MovieTime/static/js/runtime-main.891ede10.js"
+  }
+]);
